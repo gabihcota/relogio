07380ac9ab9beb0eea6aa44cc7a248d30b8bfeeb
gabihcota <165922072+gabihcota@users.noreply.github.com>
2025-04-25 09:00:30 -0300

    Update main.js

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2025-04-05T00:00:00");
+const tempoObjetivo1 = new Date("2025-04-05T01:00:00");
 const tempoObjetivo2 = new Date("2025-04-05T00:00:00");
 const tempoObjetivo3 = new Date("2025-04-30T00:00:00");
 const tempoObjetivo4 = new Date("2025-04-01T00:00:00");
